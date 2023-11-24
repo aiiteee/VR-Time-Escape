2a1ec57e4e27444c605e3bac1b867b46f530a78f
aiiteee <pernille.mikkelsen@outlook.dk>
2023-11-24 10:54:44 +0100

    Knap virker (test)

---
+++ Assets/ButtonInteractable.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class ButtonInteractable : MonoBehaviour
+{
+    public GameObject button;
+    public UnityEvent onPress;
+    public UnityEvent onRelease;
+    GameObject presser;
+    bool isPressed;
+
+    void Start()
+    {
+        isPressed = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!isPressed)
+        {
+            button.transform.localPosition = new Vector3(0, 0.003f, 0);
+            presser = other.gameObject;
+            onPress.Invoke();
+            isPressed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == presser)
+        {
+            //SceneManager.LoadScene("Dinosaur");
+            button.transform.localPosition = new Vector3(0, 0.015f, 0);
+            onRelease.Invoke();
+            isPressed = false;
+        }
+    }
+
+    public void LoadDinosaur()
+    {
+        SceneManager.LoadScene("Dinosaur");
+    }
+
+}
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
 
 public class GameManager : MonoBehaviour
 {
@@ -14,5 +17,10 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void LoadDinosaur()
+    {
+        SceneManager.LoadScene("Dinosaur");
     }
 }
