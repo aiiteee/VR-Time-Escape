fa945cb22e5451a4068c3cf2fcfc034a68bddaad
YourselfDK <johan.bergmann@hotmail.com>
2023-11-24 13:58:16 +0100

    Timer til Dino

---
+++ Assets/Scripts/Countdown_Timer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown_Timer : MonoBehaviour
+{
+   
+
+	float currentTimeSeconds =0;
+	float startingTimeSeconds =59;
+	
+	float currentTimeMinutes =0;
+	float startingMinutes =29;
+	
+	[SerializeField]
+	Text countdownTextSeconds;
+	[SerializeField]
+	Text countdownMinutes;
+	
+   // Start is called before the first frame update
+    void Start()
+    {
+        currentTimeSeconds = startingTimeSeconds;
+		currentTimeMinutes = startingMinutes;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTimeSeconds > 0)
+		{
+		currentTimeSeconds -= 1 * Time.deltaTime;
+		}
+		
+	else
+	{
+			currentTimeMinutes -= 1;
+			currentTimeSeconds = startingTimeSeconds;
+	}
+	countdownTextSeconds.text = currentTimeSeconds.ToString("0");
+	countdownMinutes.text = currentTimeMinutes.ToString("0");
+    }
+}
